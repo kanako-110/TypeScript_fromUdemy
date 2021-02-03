4b248bb025bb45eafb6c13f4f16a821b1e3392f9
kanako-110 <kanako.110.ito@gmail.com>
2021-02-03 21:03:59 +0900

    decorator

--- app.ts
+++ app.ts
@@ -9,11 +9,19 @@ function Logger(longString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-	return function (_: Function) {
-		const hookEl = document.getElementById(hookId);
-		if (hookEl) {
-			hookEl.innerHTML = template;
-		}
+	console.log("TEMPLATE FACTORY");
+	return function <T extends {new(...args: any[]): {name: string}}> (originalConstructor: T) {
+		return class extends originalConstructor {
+			constructor(..._: any[]) {
+				super();
+				console.log("Rendering Template");
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector("h1")!.textContent = this.name;
+				}
+			}
+		};
 	};
 }
 
@@ -30,3 +38,48 @@ class PERSON {
 const pers = new PERSON();
 
 console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+	console.log("Log Func Here");
+	console.log(target, propertyName);
+}
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Log2 here");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Log3 here");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log("Log4 here");
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+class Product {
+	@Log
+	title: string;
+	private _price: number;
+
+	@Log2
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val;
+		} else {
+			throw new Error("Invalid Price");
+		}
+	}
+
+	constructor(t: string, p: number) {
+		(this.title = t), (this._price = p);
+	}
+	@Log3
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
+	}
+}
--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 // まず使うfuncを定義
 function Logger(longString) {
     return function (constructor) {
@@ -14,11 +17,19 @@ function Logger(longString) {
     };
 }
 function WithTemplate(template, hookId) {
-    return function (_) {
-        const hookEl = document.getElementById(hookId);
-        if (hookEl) {
-            hookEl.innerHTML = template;
-        }
+    console.log("TEMPLATE FACTORY");
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log("Rendering Template");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = this.name;
+                }
+            }
+        };
     };
 }
 // @funcName
@@ -33,4 +44,52 @@ PERSON = __decorate([
 ], PERSON);
 const pers = new PERSON();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log("Log Func Here");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Log2 here");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log("Log3 here");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log("Log4 here");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(t, p) {
+        (this.title = t), (this._price = p);
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid Price");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
 //# sourceMappingURL=app.js.map
